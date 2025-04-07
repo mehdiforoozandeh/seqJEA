71c7a0d30e33f3dae7898667f5bcbb01e5a1c5fd
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-04-06 20:32:24 -0400

    ...

--- dino_dna.py
+++ dino_dna.py
@@ -292,7 +292,7 @@ if __name__ == "__main__":
     dim_feedforward = 2 * embed_dim
     projection_dim = embed_dim
     max_len_seq = 20000  # maximum sequence length for dataset
-    context_length = 1000  # model's context length (max_len for transformer)
+    context_length = 500  # model's context length (max_len for transformer)
     dropout = 0.1
     num_epochs = 100
     n_subseq = 2
