8615a48f0f968d0b79787716cb3981645cfb6ef4
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-04-13 13:11:58 -0400

    ...

--- dino_dna.py
+++ dino_dna.py
@@ -346,6 +346,7 @@ if __name__ == "__main__":
     num_layers = num_layers // 2
     context_length = context_length // 2
     max_len_seq = max_len_seq // 2
+    num_layers = num_layers // 2
     batch_size *= 8
 
     # Load tokenizer and obtain token IDs for special tokens.
